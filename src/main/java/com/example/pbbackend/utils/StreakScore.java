05d93667b5ca8cabce956b28c806a7888962bdd2
Kritika <kritz.rs@gmail.com>
2020-09-12 14:35:55 +0530

    Score calculation logic done

--- src/main/java/com/example/pbbackend/utils/StreakScore.java
+++
@@ -1,4 +0,0 @@
-package com.example.pbbackend.utils;
-
-public class StreakScore {
-}
--- src/main/java/com/example/pbbackend/utils/TaskScore.java
+++ src/main/java/com/example/pbbackend/utils/TaskScore.java
@@ -1,19 +1,33 @@
 package com.example.pbbackend.utils;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class TaskScore {
     private static final int basePointLevel = 10;
     private static final int basePenaltyLevel = 5;
-    public static int taskScoreCalculation(ArrayList<Integer> taskDistribution, ArrayList<Integer> taskCompletion, boolean familyTime){
+
+    //Hashmap contains task percentage corresponding to 1/0 if they were done or not
+    // consists the score from the database
+    // returns the final score directly store in database
+    public static int taskScoreCalculation(HashMap<Double,Integer> tasks, boolean familyTime,int score){
         int percentageOfWorkDone = 0 ;
-        for(int i = 0 ; i < taskDistribution.size(); i++){
-            percentageOfWorkDone += taskDistribution.get(i)*taskCompletion.get(i);
+        Iterator itr = tasks.entrySet().iterator();
+        while (itr.hasNext()) {
+            Map.Entry mapElement = (Map.Entry) itr.next();
+            percentageOfWorkDone += (double) mapElement.getKey() * (int) mapElement.getValue();
         }
-        int score = (percentageOfWorkDone*basePointLevel) - ((1 - percentageOfWorkDone)* basePenaltyLevel);
+
+        score = (percentageOfWorkDone*basePointLevel) - ((1 - percentageOfWorkDone)* basePenaltyLevel);
         if(familyTime){
-            score += 2
+            score += 2;
         }
         return score;
     }
+
+    public static int streakScoreCalculation(int score, int noOfStreakDays){
+        return score + noOfStreakDays;
+    }
+
 }
