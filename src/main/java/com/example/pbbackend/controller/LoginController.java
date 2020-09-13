481f62872211a0c75a63474f8c98d5a7b6b13946
NikitaMasand <masandnikita@gmail.com>
2020-09-13 19:06:01 +0530

    commit hai uye

--- src/main/java/com/example/pbbackend/controller/LoginController.java
+++ src/main/java/com/example/pbbackend/controller/LoginController.java
@@ -18,7 +18,7 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    @RequestMapping(value={"/","/login"}, method = RequestMethod.GET)
     public ModelAndView login(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
